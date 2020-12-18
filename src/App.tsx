ab5fa99add6336085b67d5268c6833916ed5c9c5
nirmala <nirmala@surf.net.np>
2020-12-19 00:12:51 +0100

    Add box and grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Box from "./component/Box/Box";
+import Grid from "./component/Grid/Grid";
 
 function App() {
-  return <div className="App">Hello app</div>;
+  return (
+    <div className="App">
+      <Grid container>
+        <Grid item>
+          <Box>Box1</Box>
+        </Grid>
+
+        <Grid item>
+          <Box>Box2</Box>
+        </Grid>
+
+        <Grid item>
+          <Box>Box3</Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
