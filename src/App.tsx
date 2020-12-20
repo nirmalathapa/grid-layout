73abcd3c5a8ef82c0314024f060a87dafbcb22f8
nirmala <nirmala@surf.net.np>
2020-12-21 00:00:13 +0100

    Add grid ruler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Box from "./component/Box/Box";
 import Grid from "./component/Grid/Grid";
+import GridRuler from "./component/GridRuler/GridRuler";
 
 function App() {
   return (
-    <div className="App" style={{margin: "26px"}}>
+    <div className="App" style={{margin: "16px", position: "relative"}}>
+      <GridRuler spacing="sm"></GridRuler>
+    
       <Grid container spacing="sm">
         <Grid item cols={6}>
           <Box>Box1</Box>
