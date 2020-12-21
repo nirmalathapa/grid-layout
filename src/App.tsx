eafc1be9b64f601caf828658758c2819bef9e2a6
nirmala <nirmala@surf.net.np>
2020-12-21 11:32:15 +0100

    Add justifyContent and alignItems

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,30 @@ import GridRuler from "./component/GridRuler/GridRuler";
 
 function App() {
   return (
-    <div className="App" style={{margin: "16px", position: "relative"}}>
+    <div
+      className="App"
+      style={{margin: "16px", position: "relative", height: "100vh"}}>
       <GridRuler spacing="sm"></GridRuler>
-    
-      <Grid container spacing="sm">
-        <Grid item cols={6}>
+
+      <Grid
+        container
+        spacing="sm"
+        justifyContent="center"
+        alignItems="center"
+        style={{height: "100%"}}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box1</Box>
         </Grid>
 
-        <Grid item cols={6}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box2</Box>
         </Grid>
 
-        <Grid item cols={6}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box3</Box>
         </Grid>
 
-        <Grid item cols={6}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box4</Box>
         </Grid>
       </Grid>
